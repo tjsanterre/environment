1b9dd058638248cdf2e4431bd2cae7735c42d676
Tyler Santerre <tyler.santerre@gmail.com>
2015-06-08 09:16:14 -0400

    Add initial vim configuration

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -2,5 +2,17 @@
 
 install_dir=$(cd "$(dirname "$0")"; pwd);
 
-# Link the scripts in the link directory.
-ln -s $install_dir/link/bashrc $HOME/.bashrc
+# Install bashrc.
+ln -s -f $install_dir/link/bashrc $HOME/.bashrc
+
+# Install vimrc. This includes setting up vundle.
+ln -s -f $install_dir/link/vimrc $HOME/.vimrc
+if [[ ! -d $HOME/.vim/colors ]]; then
+    mkdir -p $HOME/.vim/colors
+    ln -s $install_dir/link/zenburn.vim $HOME/.vim/colors/zenburn.vim
+fi
+if [[ ! -d $HOME/.vim/bundle ]]; then
+    mkdir -p $HOME/.vim/bundle
+    git clone http://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle.vim
+fi
+
