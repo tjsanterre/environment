1c225d8c11ea926e262cf49737a947d995713854
Tyler Santerre <tyler.santerre@gmail.com>
2016-08-16 08:06:01 -0400

    Update vim, rust env, and add colorized man pages

--- scripts/source/misc.bash
+++ scripts/source/misc.bash
@@ -7,3 +7,17 @@ function use_env_libs() {
 function use_env_pkgconfig() {
     export PKG_CONFIG_PATH="$HOME/environment/lib/pkgconfig:$PKG_CONFIG_PATH"
 }
+
+# Colorize man pages
+function man() {
+        #LESS_TERMCAP_md=$'\e[1;38;2;52;101;164m' \
+        #LESS_TERMCAP_so=$'\e[1;40;92m' \
+    env \
+        LESS_TERMCAP_md=$'\e[1;34m' \
+        LESS_TERMCAP_me=$'\e[0m' \
+        LESS_TERMCAP_se=$'\e[0m' \
+        LESS_TERMCAP_so=$'\e[34m' \
+        LESS_TERMCAP_ue=$'\e[0m' \
+        LESS_TERMCAP_us=$'\e[1;35m' \
+        man "$@"
+}
