52eed0ec0f52e49d060f67bffc871fa84706f7bc
Tyler Santerre <tyler.santerre@gmail.com>
2016-08-31 11:01:46 -0400

    Minor updates
    Adds spell checking to vim with shortcut to toggle on and off
    Configures gruvbox colors and man page colors

--- scripts/source/misc.bash
+++ scripts/source/misc.bash
@@ -16,7 +16,7 @@ function man() {
         LESS_TERMCAP_md=$'\e[1;34m' \
         LESS_TERMCAP_me=$'\e[0m' \
         LESS_TERMCAP_se=$'\e[0m' \
-        LESS_TERMCAP_so=$'\e[34m' \
+        LESS_TERMCAP_so=$'\e[0;33m' \
         LESS_TERMCAP_ue=$'\e[0m' \
         LESS_TERMCAP_us=$'\e[1;35m' \
         man "$@"
