8092f8cd2f75bf19a84530413f9fc19b0b127bb7
Tyler Santerre <tyler.santerre@gmail.com>
2016-09-22 14:05:44 -0400

    Add tmux.conf to installed (linked) files

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 # Installs all scripts in this directory.
 
-install_dir=$(cd "$(dirname "$0")"; pwd);
+script_dir=$(cd "$(dirname "$0")"; pwd);
+install_dir="${script_dir}/install"
 
 # Install bashrc.
 ln -s -f $install_dir/link/bashrc $HOME/.bashrc
@@ -9,13 +10,18 @@ ln -s -f $install_dir/link/bashrc $HOME/.bashrc
 ln -s -f $install_dir/link/vimrc $HOME/.vimrc
 if [[ ! -d $HOME/.vim/colors ]]; then
     mkdir -p $HOME/.vim/colors
-    ln -s $install_dir/link/zenburn.vim $HOME/.vim/colors/zenburn.vim
 fi
+ln -s -f $install_dir/link/zenburn.vim $HOME/.vim/colors/zenburn.vim
+
 if [[ ! -d $HOME/.vim/bundle ]]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle.vim
 fi
 
+# Install tmux.
+ln -s -f $install_dir/link/tmux.conf $HOME/.tmux.conf
+
 # Install sbuild, customized scons build for c++.
 mkdir -p $HOME/.scons/site_scons
 ln -s -f $install_dir/link/sbuild.py $HOME/.scons/site_scons/sbuild.py
+
