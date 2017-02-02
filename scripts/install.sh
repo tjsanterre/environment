3a585cf92e52f45a234f7d9a3b569b05717fa0d6
Tyler Santerre <tyler.santerre@gmail.com>
2017-02-02 12:25:47 -0500

    Simplify link scripts
    Add gitconfig as a linked script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,25 +3,22 @@
 script_dir=$(cd "$(dirname "$0")"; pwd);
 install_dir="${script_dir}/install"
 
-# Install bashrc.
-ln -s -f $install_dir/link/bashrc $HOME/.bashrc
+function install_via_symlink() {
+    ln -s -f $install_dir/link/$1 $2
+}
+
+install_via_symlink bashrc $HOME/.bashrc
+install_via_symlink tmux.conf $HOME/.tmux.conf
+install_via_symlink gitconfig $HOME/.gitconfig
 
 # Install vimrc. This includes setting up vundle.
-ln -s -f $install_dir/link/vimrc $HOME/.vimrc
-if [[ ! -d $HOME/.vim/colors ]]; then
-    mkdir -p $HOME/.vim/colors
-fi
-ln -s -f $install_dir/link/zenburn.vim $HOME/.vim/colors/zenburn.vim
-
+install_via_symlink vimrc $HOME/.vimrc
 if [[ ! -d $HOME/.vim/bundle ]]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle.vim
 fi
 
-# Install tmux.
-ln -s -f $install_dir/link/tmux.conf $HOME/.tmux.conf
-
 # Install sbuild, customized scons build for c++.
 mkdir -p $HOME/.scons/site_scons
-ln -s -f $install_dir/link/sbuild.py $HOME/.scons/site_scons/sbuild.py
+install_via_symlink sbuild.py $HOME/.scons/site_scons/sbuild.py
 
