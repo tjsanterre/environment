f5b13ed49ee097f4373e75306fcd62f5ca47490c
Tyler Santerre <tyler.santerre@gmail.com>
2017-02-21 14:24:24 -0500

    Adds neovim support.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,10 @@ script_dir=$(cd "$(dirname "$0")"; pwd);
 install_dir="${script_dir}/install"
 
 function install_via_symlink() {
+    local parent_dir=$(dirname $2)
+    if [[ ! -d "$parent_dir" ]]; then
+        mkdir -p "$parent_dir"
+    fi
     ln -s -f $install_dir/link/$1 $2
 }
 
@@ -17,6 +21,14 @@ if [[ ! -d $HOME/.vim/bundle ]]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle.vim
 fi
+
+# Install neovim
+install_via_symlink init.nvim $XDG_CONFIG_HOME/nvim/init.vim
+if [[ ! -f $XDG_DATA_HOME/nvim/site/autoload/plug.vim ]]; then
+	curl -fLo $XDG_DATA_HOME/nvim/site/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 
 # Install sbuild, customized scons build for c++.
 mkdir -p $HOME/.scons/site_scons
