48de583352030307d1c320f791640f2f7ca020b8
Tyler Santerre <tylers@activestate.com>
2018-06-07 11:38:20 -0400

    Add vscode user settings file to environment

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,9 +11,19 @@ function install_via_symlink() {
     ln -s -f $install_dir/link/$1 $2
 }
 
+
+# Install all files that only require symlink.
 install_via_symlink bashrc $HOME/.bashrc
 install_via_symlink tmux.conf $HOME/.tmux.conf
 install_via_symlink gitconfig $HOME/.gitconfig
+
+
+# Install vscode user settings.
+if [[ -d $HOME/.config/Code/User ]]; then
+    mkdir -p $HOME/.config/Code/User
+fi
+install_via_symlink vscode_settings.json $HOME/.config/Code/User/settings.json
+
 
 # Install vimrc. This includes setting up vundle.
 install_via_symlink vimrc $HOME/.vimrc
@@ -22,11 +32,12 @@ if [[ ! -d $HOME/.vim/bundle ]]; then
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle.vim
 fi
 
+
 # Install neovim
 install_via_symlink init.nvim $XDG_CONFIG_HOME/nvim/init.vim
 if [[ ! -f $XDG_DATA_HOME/nvim/site/autoload/plug.vim ]]; then
-	curl -fLo $XDG_DATA_HOME/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo $XDG_DATA_HOME/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 
