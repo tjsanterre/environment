a669b42f5b0dd06cec82919664fcac3321d8a9a1
Tyler Santerre <tsanterre@idirectgov.com>
2018-06-26 05:47:57 -0400

    Disable nvim and scons build

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,14 +31,14 @@ fi
 
 
 # Install neovim
-install_via_symlink init.nvim $XDG_CONFIG_HOME/nvim/init.vim
-if [[ ! -f $XDG_DATA_HOME/nvim/site/autoload/plug.vim ]]; then
-    curl -fLo $XDG_DATA_HOME/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+#install_via_symlink init.nvim $HOME/.config/nvim/init.vim
+#if [[ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]]; then
+#    curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#fi
 
 
 # Install sbuild, customized scons build for c++.
-mkdir -p $HOME/.scons/site_scons
-install_via_symlink sbuild.py $HOME/.scons/site_scons/sbuild.py
+#mkdir -p $HOME/.scons/site_scons
+#install_via_symlink sbuild.py $HOME/.scons/site_scons/sbuild.py
 
