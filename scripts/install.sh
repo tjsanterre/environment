5f7a4901f9f1076b18dbefd53f05addafeac8a45
Tyler Santerre <tylers@activestate.com>
2023-11-20 11:39:42 -0500

    get back to a single branch

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,16 +11,11 @@ function install_via_symlink() {
     ln -s -f $install_dir/link/$1 $2
 }
 
-
 # Install all files that only require symlink.
 install_via_symlink bashrc $HOME/.bashrc
 install_via_symlink bash_aliases $HOME/.bash_aliases
 install_via_symlink bash_prompt $HOME/.bash_prompt
-install_via_symlink tmux.conf $HOME/.tmux.conf
 install_via_symlink gitconfig $HOME/.gitconfig
-install_via_symlink vscode_settings.json $HOME/.config/Code/User/settings.json
-install_via_symlink powerline-go.json $HOME/.config/powerline-go/theme.json
-
 
 # Install vimrc. This includes setting up vundle.
 install_via_symlink vimrc $HOME/.vimrc
@@ -28,17 +23,3 @@ if [[ ! -d $HOME/.vim/bundle ]]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/vundle.vim
 fi
-
-
-# Install neovim
-#install_via_symlink init.nvim $HOME/.config/nvim/init.vim
-#if [[ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]]; then
-#    curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#fi
-
-
-# Install sbuild, customized scons build for c++.
-#mkdir -p $HOME/.scons/site_scons
-#install_via_symlink sbuild.py $HOME/.scons/site_scons/sbuild.py
-
---
+++ scripts/source/activestate.bash
@@ -0,0 +1,22 @@
+# ActiveState specific environment and functions.
+
+# local
+export INV_DB_URL=postgresql://pb_admin:2sPkz0bZKmlz83W9AdO8Cd0zV@localhost/inventory-api-v1
+
+#alias bazel='bazelisk'
+#alias minik='minikube kubectl --'
+export KT_BROKERS_DEV=kafka-development.activestate.build:31090,kafka-development.activestate.build:31091,kafka-development.activestate.build:31092
+export KT_BROKERS_PROD=kafka-production.activestate.build:31090,kafka-production.activestate.build:31091,kafka-production.activestate.build:31092
+export KT_BROKERS="$KT_BROKERS_PROD"
+
+# Try here. Delete if it becomes a problem.
+export PATH=$HOME/code/activestate/TheHomeRepot/third_party/bin:$PATH
+
+function thr() {
+    #kperl
+    export PATH=$PWD/third_party/bin:$PATH
+}
+
+function dashboard() {
+    export PATH=$PWD/service/dashboard/node_modules/.bin:$PATH
+}
