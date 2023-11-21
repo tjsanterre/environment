ae13a25714a5abc99b6685cffc6553c38d7108e0
Tyler Santerre <tylers@activestate.com>
2023-11-21 11:06:05 -0500

    install tools

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,3 +25,34 @@ if [[ ! -d $HOME/.vim/bundle ]]; then
 fi
 
 # Install useful tools.
+BIN_DIR=$HOME/.local/bin
+mkdir -p $BIN_DIR
+
+# jq
+if [[ ! -f $BIN_DIR/jq ]]; then
+    echo 'installing latest jq'
+    curl -sL -o $BIN_DIR/jq https://github.com/jqlang/jq/releases/latest/download/jq-linux-amd64
+    chmod +x $BIN_DIR/jq
+fi
+
+# mob
+if [[ ! -f $BIN_DIR/mob ]]; then
+    curl -sL install.mob.sh | sh -s - --user
+fi
+
+# go tools
+if command -v go 2>&1 >/dev/null; then
+    # powerline
+    if [[ ! -f $GOPATH/bin/powerline-go ]]; then
+        echo "installing powerline-go to $GOPATH/bin"
+        go install github.com/justjanne/powerline-go@latest
+    fi
+
+    # kt
+    if [[ ! -f $GOPATH/bin/kt ]]; then
+        echo "installing kt to $GOPATH/bin"
+        go install github.com/fgeller/kt/v14@latest
+    fi
+else
+    echo "go not installed; skipping go tools"
+fi
